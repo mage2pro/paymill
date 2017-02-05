41b31211db0db22c71b20f98a6ca2b698cd22cc1
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-05 20:09:52 +0300

    no message

---
+++ Source/Prefill/With3DS.php
@@ -0,0 +1,16 @@
+<?php
+namespace Dfe\Paymill\Source\Prefill;
+final class With3DS extends \Df\Config\SourceT {
+	/**
+	 * 2017-02-05
+	 * https://mage2.pro/t/2639
+	 * @override
+	 * @see \Df\Config\Source::map()
+	 * @used-by \Df\Config\Source::toOptionArray()
+	 * @return array(string => string)
+	 */
+	protected function map() {return [
+		'4012888888881881' => 'Visa'
+		,'5169147129584558' => 'MasterCard'
+	];}
+}
---
+++ Source/Prefill/Without3DS.php
@@ -0,0 +1,22 @@
+<?php
+namespace Dfe\Paymill\Source\Prefill;
+final class Without3DS extends \Df\Config\SourceT {
+	/**
+	 * 2017-02-05
+	 * https://mage2.pro/t/2639
+	 * @override
+	 * @see \Df\Config\Source::map()
+	 * @used-by \Df\Config\Source::toOptionArray()
+	 * @return array(string => string)
+	 */
+	protected function map() {return [
+		'4111111111111111' => 'Visa'
+		,'5500000000000004' => 'MasterCard'
+		,'340000000000009' => 'American Express'
+		,'3530111333300000' => 'JCB'
+		,'6759000000000000' => 'Maestro UK'
+		,'4973010000000004' => 'Carte Bleue'
+		,'30000000000004' => 'Diners Club'
+		,'6011111111111117' => 'Discover'
+	];}
+}
