250e5507065fdbb8ed1f717d95205799af29c12a
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-05 20:25:02 +0300

    no message

--- Method.php
+++ Method.php
@@ -26,6 +26,16 @@ class Method extends \Df\StripeClone\Method {
 	/**
 	 * 2017-02-05
 	 * @override
+	 * @see \Df\StripeClone\Method::apiChargeCreate()
+	 * @used-by \Df\StripeClone\Method::chargeNew()
+	 * @param array(string => mixed) $params
+	 * @return object
+	 */
+	final protected function apiChargeCreate(array $params) {return null;}
+
+	/**
+	 * 2017-02-05
+	 * @override
 	 * @see \Df\StripeClone\Method::responseToArray()
 	 * @used-by \Df\StripeClone\Method::transInfo()
 	 * @param object $response
