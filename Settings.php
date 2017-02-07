a801c4d7871d638870f5c5d1f655c9ffae2bb2ac
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-07 06:26:43 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -2,4 +2,23 @@
 // 2017-02-05
 namespace Dfe\Paymill;
 /** @method static Settings s() */
-final class Settings extends \Df\StripeClone\Settings {}
+final class Settings extends \Df\StripeClone\Settings {
+	/**
+	 * 2017-02-07
+	 * @override
+	 * @see \Df\Payment\Settings\BankCard::prefill()
+	 * @used-by \Df\Payment\ConfigProvider\BankCard::config()
+	 * @return string|null
+	 */
+	public function prefill() {return $this->v("prefill{$this->test3DS('With', 'Without')}3DS");}
+
+	/**
+	 * 2017-02-07
+	 * «Test with the 3D Secure validation?»
+	 * Первый аргумент — для «да», второй — для «нет».
+	 * @param mixed[] ...$args [optional]
+	 * @used-by prefill()
+	 * @return bool
+	 */
+	private function test3DS(...$args) {return df_b($args, $this->b());}
+}
