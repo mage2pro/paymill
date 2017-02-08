f1b5ef2679bd6589098f27e3edf445d77b2799b1
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-08 16:28:11 +0300

    no message

--- T/Charge.php
+++ T/Charge.php
@@ -4,14 +4,20 @@ namespace Dfe\Paymill\T;
 use Paymill\Models\Request\Payment as lPayment;
 use Paymill\Models\Response\Base as lResponseBase;
 use Paymill\Models\Response\Payment as lResponsePayment;
+use Paymill\Request as lRequest;
 final class Charge extends TestCase {
 	/**
 	 * @test
 	 * 2017-02-08
 	 */
 	public function t01() {
+		/** @var lRequest $req */
+		$req = $this->r();
 		/** @var lResponseBase|lResponsePayment $resp */
-		$resp = $this->r()->create((new lPayment)->setToken($this->token()));
-		echo $resp->getId();
+		$resp = $req->create((new lPayment)->setToken($this->token()));
+		echo df_json_encode_pretty([
+			'Response ID' => $resp->getId()
+			,'Response' => $req->getLastResponse()
+		]);
 	}
 }
