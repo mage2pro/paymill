ec63c17fb148bd2e095545aed3883a5b55b93b85
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-10 20:54:23 +0300

    no message

--- Facade/Charge.php
+++ Facade/Charge.php
@@ -5,6 +5,16 @@ final class Charge extends \Df\StripeClone\Facade\Charge {
 	/**
 	 * 2017-02-10
 	 * @override
+	 * @see \Df\StripeClone\Facade\Charge::capturePreauthorized()
+	 * @used-by \Df\StripeClone\Method::charge()
+	 * @param string $id
+	 * @return object
+	 */
+	public function capturePreauthorized($id) {return null;}
+
+	/**
+	 * 2017-02-10
+	 * @override
 	 * @see \Df\StripeClone\Facade\Charge::create()
 	 * @used-by \Df\StripeClone\Method::chargeNew()
 	 * @param array(string => mixed) $p
