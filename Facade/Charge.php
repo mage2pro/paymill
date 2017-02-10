1581fb3861891fd35466f6c6fdd4bdfb0fe9a9dd
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-10 23:30:20 +0300

    no message

--- Facade/Charge.php
+++ Facade/Charge.php
@@ -32,19 +32,4 @@ final class Charge extends \Df\StripeClone\Facade\Charge {
 	 * @return string
 	 */
 	public function id($c) {return $c->getId();}
-
-	/**
-	 * 2017-02-10
-	 * Пока этот метод используется только в сценарии возврата.
-	 * Метод должен вернуть идентификатор операции (не платежа!) в платёжной системе.
-	 * Мы записываем его в БД и затем при обработке оповещений от платёжной системы
-	 * смотрим, не было ли это оповещение инициировано нашей же операцией,
-	 * и если было, то не обрабатываем его повторно.
-	 * @override
-	 * @see \Df\StripeClone\Facade\Charge::transId()
-	 * @used-by \Df\StripeClone\Method::_refund()
-	 * @param object $c
-	 * @return string
-	 */
-	public function transId($c) {return '';}
 }
