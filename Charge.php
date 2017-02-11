121ce06652340483f835ebce36250cf4eb7bdc84
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-11 04:30:29 +0300

    no message

--- Charge.php
+++ Charge.php
@@ -2,8 +2,23 @@
 namespace Dfe\Paymill;
 use Df\Core\Exception as DFE;
 use Magento\Sales\Model\Order\Payment as OP;
-/**
- * 2017-02-09
- * @method Settings ss()
- */
-abstract class Charge extends \Df\StripeClone\Charge {}
+// 2017-02-09
+final class Charge extends \Df\StripeClone\Charge {
+	/**
+	 * 2017-02-11
+	 * @override
+	 * @see \Df\StripeClone\Charge::_request()
+	 * @used-by \Df\StripeClone\Charge::request()
+	 * @return array(string => mixed)
+	 */
+	protected function _request() {/** @var Settings $s */ $s = $this->ss(); return [];}
+
+	/**
+	 * 2017-02-11
+	 * @override
+	 * @see \Df\StripeClone\Charge::customerParams()
+	 * @used-by \Df\StripeClone\Charge::newCard()
+	 * @return array(string => mixed)
+	 */
+	protected function customerParams() {return [];}
+}
