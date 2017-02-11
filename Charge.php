c1b9c4c02c4a1a4424d8e4a03ee4f5be2349d9c1
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-11 07:17:14 +0300

    no message

--- Charge.php
+++ Charge.php
@@ -15,6 +15,17 @@ final class Charge extends \Df\StripeClone\Charge {
 
 	/**
 	 * 2017-02-11
+	 * Идентификаторы банковских карт (в терминологии Paymill - «Payment») имеют вид
+	 * «pay_ddcc9210289ede708c97eb67».
+	 * @override
+	 * @see \Df\StripeClone\Charge::cardIdPrefix()
+	 * @used-by \Df\StripeClone\Charge::usePreviousCard()
+	 * @return mixed
+	 */
+	protected function cardIdPrefix() {return 'pay';}
+
+	/**
+	 * 2017-02-11
 	 * @override
 	 * @see \Df\StripeClone\Charge::customerParams()
 	 * @used-by \Df\StripeClone\Charge::newCard()
