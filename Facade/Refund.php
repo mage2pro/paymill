6d604a1ed01964e6714027949f957bca2d172f2f
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-14 11:45:53 +0300

    no message

--- Facade/Refund.php
+++ Facade/Refund.php
@@ -10,12 +10,16 @@ final class Refund extends \Df\StripeClone\Facade\Refund {
 	 * смотрим, не было ли это оповещение инициировано нашей же операцией,
 	 * и если было, то не обрабатываем его повторно.
 	 * 2017-02-14
+	 * Замечание №1
 	 * Этот же идентификатор должен возвращать @see \Dfe\Paymill\Webhook\Refund\Succeeded::eTransId()
+	 * Замечание №2
+	 * Пример результата: «refund_2c9cd9a13357f2454522»
+	 * [Paymill] An example of a refund response: https://mage2.pro/t/2751
 	 * @override
 	 * @see \Df\StripeClone\Facade\Refund::transId()
 	 * @used-by \Df\StripeClone\Method::_refund()
 	 * @param object $r
 	 * @return string
 	 */
-	function transId($r) {return '';}
+	function transId($r) {return $r->getId();}
 }
