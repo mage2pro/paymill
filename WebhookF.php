ee7efadd346563e3bfa0a4dd903909f384ab7652
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-14 03:02:04 +0300

    no message

---
+++ WebhookF.php
@@ -0,0 +1,15 @@
+<?php
+// 2017-02-14
+namespace Dfe\Paymill;
+final class WebhookF extends \Df\StripeClone\WebhookF {
+	/**             
+	 * 2017-02-14
+	 * @override
+	 * @see \Df\StripeClone\WebhookF::typeKey()
+	 * @used-by \Df\StripeClone\WebhookF::_class()
+	 * @return string
+	 */
+	protected function typeKey() {return '';}
+}
+
+
