57496551ca1290bc1043ec8187856aecd8a1df51
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-09 20:30:25 +0300

    no message

--- WebhookF.php
+++ WebhookF.php
@@ -7,8 +7,8 @@ final class WebhookF extends \Df\StripeClone\WebhookF {
 	 * [Paymill] An example of the «transaction.created» event (being sent to a webhook)
 	 * https://mage2.pro/t/2743
 	 * @override
-	 * @see \Df\StripeClone\WebhookF::typeKey()
-	 * @used-by \Df\StripeClone\WebhookF::type()
+	 * @see \Df\Payment\WebhookF\Json::typeKey()
+	 * @used-by \Df\Payment\WebhookF\Json::type()
 	 * @return string
 	 */
 	protected function typeKey() {return 'event/event_type';}
