fe58d0847684b1e54fb73ab168ef0377adcd146e
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-26 18:46:40 +0300

    no message

--- W/Handler/Refund/Succeeded.php
+++ W/Handler/Refund/Succeeded.php
@@ -1,6 +1,6 @@
 <?php
 namespace Dfe\Paymill\W\Handler\Refund;
-use Df\StripeClone\W\Strategy\Charge\Refunded as Strategy;
+use \Df\StripeClone\W\Strategy\Refunded as Strategy;
 // 2017-02-14
 // Оповещение «refund.succeeded» приходит
 // при выполнении операции «refund» из административного интерфейса Paymill.
