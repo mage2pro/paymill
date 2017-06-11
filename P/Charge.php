36fca8958e0f714b9c6ecdda4f99d97dd128a8b2
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-11 17:44:50 +0300

    1.4.9

--- P/Charge.php
+++ P/Charge.php
@@ -6,6 +6,16 @@ use Magento\Sales\Model\Order\Payment as OP;
 final class Charge extends \Df\StripeClone\P\Charge {
 	/**
 	 * 2017-02-11
+	 * 2017-02-18 Ключ, значением которого является токен банковской карты.
+	 * @override
+	 * @see \Df\StripeClone\P\Charge::k_CardId()
+	 * @used-by \Df\StripeClone\P\Charge::request()
+	 * @return string
+	 */
+	function k_CardId() {return self::K_CARD;}
+	
+	/**
+	 * 2017-02-11
 	 * Идентификаторы банковских карт (в терминологии Paymill - «Payment») имеют вид
 	 * «pay_ddcc9210289ede708c97eb67».
 	 * @override
@@ -14,16 +24,6 @@ final class Charge extends \Df\StripeClone\P\Charge {
 	 * @return string
 	 */
 	protected function cardIdPrefix() {return 'pay';}
-
-	/**
-	 * 2017-02-11
-	 * 2017-02-18 Ключ, значением которого является токен банковской карты.
-	 * @override
-	 * @see \Df\StripeClone\P\Charge::k_CardId()
-	 * @used-by \Df\StripeClone\P\Charge::request()
-	 * @return string
-	 */
-	protected function k_CardId() {return self::K_CARD;}
 
 	/**
 	 * 2017-02-18
