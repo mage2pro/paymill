ece1c22a81753c48c2f93f60439dd70252db58b0
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-26 15:50:29 +0300

    no message

--- Source/Prefill/With3DS.php
+++ Source/Prefill/With3DS.php
@@ -2,14 +2,13 @@
 namespace Dfe\Paymill\Source\Prefill;
 final class With3DS extends \Dfe\Paymill\Source\Prefill {
 	/**
-	 * 2017-02-05
-	 * https://mage2.pro/t/2639
+	 * 2017-02-05 https://mage2.pro/t/2639
 	 * @override
 	 * @see \Df\Config\Source::map()
 	 * @used-by \Df\Config\Source::toOptionArray()
 	 * @return array(string => string)
 	 */
-	protected function map() {return self::addKeysToValues([
+	protected function map():array {return self::addKeysToValues([
 		'4012888888881881' => 'Visa'
 		,'5169147129584558' => 'MasterCard'
 		,self::ERROR => 'Error'
